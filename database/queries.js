ab793eaea59a9e2c9bcca48dd55a47f99012c5a6
Mohak Chugh <me.mohakchugh@gmail.com>
2020-01-31 14:31:08 +0530

    queries template added

---
+++ database/queries.js
@@ -0,0 +1,202 @@
+const graphqlrequest = require('graphql-request')
+const GraphQLClient = graphqlrequest.GraphQLClient
+
+const insertUser = async () => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getUser = async () => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getUserPassword = async (data) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const insertProblem = async (description, title, areaid, userid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+
+const getProblem = async () => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getProblemByAreaID = async (areaid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getProblemByUser = async (email) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getProblemByDepartent = async (department) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const upvoteProblem = async (problemid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const downvoteProblem = async (problemid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const deleteProblem = async (problemid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const digiSignProblem = async (problemid) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+
+const insertOfficial = async (name, password, email, phone, department, area) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
+const getOfficialPassword = async (email) => {
+    const client = new GraphQLClient('', {
+        headers: {
+            'content-type': 'application/json',
+        },
+    })
+    const query = ``
+    
+    let result = await client.request(query)
+        .then(data => { return data })
+        .catch((err) => { return err })
+    return result
+};
+
