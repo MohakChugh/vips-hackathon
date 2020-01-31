93ad65f9053e0b9e3245caf6fb8ba0e770612a33
Mohak Chugh <me.mohakchugh@gmail.com>
2020-01-31 14:09:14 +0530

    routes added

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,9 @@
 const express = require('express')
+const router = express.Router()
+
+router.route('/test')
+    .get((req, res) => {
+        res.send('Router working')
+    })
+
+module.exports = router
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 
+const routes = require('./routes/routes')
+
 const port = process.env.PORT || 8080
 const app = express()
+
+app.use('/', routes)
 
 app.route('/')
     .get((req, res) => {
