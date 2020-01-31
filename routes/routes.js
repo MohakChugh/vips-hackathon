48f97d99a6010bf6c412f619e29f3c4aa8e476da
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-01-31 14:36:06 +0530

    first

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,54 @@
 const express = require('express')
+const router = express.Router();
+
+// Users API Route
+
+router.post('/insertUser', async function (req, response) {
+
+    // get data
+    let  { data } = req.body;
+    
+    if(!(data)) {
+        return response.json({
+            error:'invalid request',
+            success: false,
+            response: false,
+        });
+    }
+
+    // Processing
+    
+    return response.json({
+        error: false,
+        success: true,
+        response: {
+            "message": "response",
+        },
+    });
+
+});
+
+router.post('/<name>', async function (req, response) {
+
+    // get data
+    let  { data } = req.body;
+    
+    if(!(data)) {
+        return response.json({
+            error:'invalid request',
+            success: false,
+            response: false,
+        });
+    }
+
+    // Processing
+    
+    return response.json({
+        error: false,
+        success: true,
+        response: {
+            "message": "response",
+        },
+    });
+
+});
